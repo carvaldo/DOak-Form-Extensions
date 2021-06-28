0644ab1a85e649c46494d2d54a365cf9bedcd454
David Carvalho <davidcarvalho.developer@gmail.com>
2021-06-27 23:47:18 -0400

    Criação da uma view (DateInputView) para entrada de data. Remoção da extensions não mais necessárias.

--- app/src/main/java/com/github/carvaldo/doakFormExtensions/EditTextViewExt.kt
+++ app/src/main/java/com/github/carvaldo/doakFormExtensions/EditTextViewExt.kt
@@ -1,12 +1,7 @@
 package com.github.carvaldo.doakFormExtensions
 
-import android.app.DatePickerDialog
 import android.util.Patterns
-import android.view.KeyEvent
-import android.widget.EditText
 import com.google.android.material.textfield.TextInputEditText
-import java.text.SimpleDateFormat
-import java.util.*
 
 fun TextInputEditText.isValidEmail(message: String? = null): Boolean {
     return if (!this.text.toString().matches(Patterns.EMAIL_ADDRESS.toRegex())) {
@@ -38,10 +33,6 @@ fun TextInputEditText.hasMinCharacters(length: Int, msgError: String? = null): B
     }
 }
 
-fun TextInputEditText.isValidDate(pattern: String, message: String? = null): Boolean {
-    TODO("Implementar validador de data através de um parse")
-}
-
 fun TextInputEditText.contentEquals(other: TextInputEditText, message: String? = null): Boolean {
     return  if (text.toString() == other.text.toString()) {
         error = null
@@ -50,34 +41,4 @@ fun TextInputEditText.contentEquals(other: TextInputEditText, message: String? =
         error = message?: "As senhas informadas não são correspondentes."
         false
     }
-}
-
-// TODO: Criar um Builder para Config do input de data.
-//    Conter:
-//        * Dia mínimo
-//        * Dia máximo
-//        * Date Pattern
-//        * Locale
-//        * Pick inicial
-fun EditText.enableDateDialog(pattern: String? = null) {
-    val dateFormat = if (pattern != null) SimpleDateFormat(pattern) else SimpleDateFormat()
-    this.setOnTouchListener { v, event ->
-        if (event.action == KeyEvent.ACTION_UP) {
-            v.performClick()
-            showDateDialog(dateFormat)
-        }
-        return@setOnTouchListener true
-    }
-}
-
-fun EditText.disableDateDialog() {
-    this.setOnTouchListener { _, _ -> return@setOnTouchListener false }
-}
-
-private fun EditText.showDateDialog(dateFormat: SimpleDateFormat) {
-    val calendar = Calendar.getInstance(Locale.getDefault())
-    DatePickerDialog(this.context, { _, year, month, dayOfMonth ->
-        calendar.set(year, month, dayOfMonth)
-        this@showDateDialog.setText(dateFormat.format(calendar.time))
-    }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
 }
---
+++ app/src/main/java/com/github/carvaldo/doakFormExtensions/view/DateInputView.kt
@@ -0,0 +1,94 @@
+package com.github.carvaldo.doakFormExtensions.view
+
+import android.app.DatePickerDialog
+import android.content.Context
+import android.os.Build
+import android.text.InputType
+import android.util.AttributeSet
+import android.view.KeyEvent
+import androidx.appcompat.widget.AppCompatEditText
+import com.github.carvaldo.doakFormExtensions.R
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Custom View para criar um campo de entrada do tipo data.
+ *
+ * @see pattern Padrão para formatação da data.
+ */
+class DateInputView : AppCompatEditText {
+    private lateinit var dateFormat: DateFormat
+    private lateinit var calendar: Calendar
+    private var pattern: String? = null
+
+    constructor(context: Context) : super(context) {
+        init(null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init(attrs, defStyle)
+    }
+
+
+    /**
+     * Converte o conteúdo da view em uma instância de Date.
+     *
+     * @return Date
+     */
+    fun toDate(): Date? = dateFormat.parse(this.text.toString())
+
+    /**
+     * Converte o conteúdo da view em uma String.
+     *
+     * @return String
+     */
+    override fun toString(): String = this.text.toString()
+
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.DateInputView, defStyle, 0
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setRawInputType(InputType.TYPE_DATETIME_VARIATION_DATE)
+        } else {
+            inputType = InputType.TYPE_DATETIME_VARIATION_DATE
+        }
+        maxLines = 1
+        pattern = a.getString(R.styleable.DateInputView_pattern)
+        calendar = Calendar.getInstance(Locale.getDefault())
+        dateFormat = if (pattern != null) {
+            SimpleDateFormat(pattern, Locale.getDefault())
+        } else {
+            SimpleDateFormat(null, Locale.getDefault())
+        }
+        enableDateDialog()
+        a.recycle()
+    }
+
+    private fun enableDateDialog() {
+        this.setOnTouchListener { v, event ->
+            if (event.action == KeyEvent.ACTION_UP) {
+                v.performClick()
+                showDateDialog()
+            }
+            return@setOnTouchListener true
+        }
+    }
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun showDateDialog() {
+        DatePickerDialog(this.context, { _, year, month, dayOfMonth ->
+            calendar.set(year, month, dayOfMonth)
+            this.setText(dateFormat.format(calendar.time))
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+    }
+}
