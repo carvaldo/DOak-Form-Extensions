256c6a08e0b907a83d741cb388567b3baf7a2e4c
David Carvalho <davidcarvalho.developer@gmail.com>
2021-06-29 00:33:31 -0400

    Criado custom view MaskInputView
    Custom view para campos com máscara.

---
+++ app/src/main/java/com/github/carvaldo/doakFormExtensions/view/MaskInputView.kt
@@ -0,0 +1,53 @@
+package com.github.carvaldo.doakFormExtensions.view
+
+import android.content.Context
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import br.com.concrete.canarinho.watcher.MascaraNumericaTextWatcher
+import com.github.carvaldo.doakFormExtensions.R
+
+open class MaskInputView : AppCompatEditText {
+    var mask: String? = null
+        set(value) {
+            field = value
+            invalidateMask()
+        }
+    private var textWatcher: TextWatcher? = null
+
+    constructor(context: Context) : super(context) {
+        init(null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init(attrs, defStyle)
+    }
+
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.MaskInputView, defStyle, 0
+        )
+        mask = a.getString(
+            R.styleable.MaskInputView_mask
+        )
+        a.recycle()
+    }
+
+    private fun invalidateMask() {
+        if (textWatcher != null) {
+            this.removeTextChangedListener(textWatcher)
+        }
+        if (mask != null) {
+            textWatcher = MascaraNumericaTextWatcher(mask)
+            this.addTextChangedListener(textWatcher)
+        }
+    }
+}
